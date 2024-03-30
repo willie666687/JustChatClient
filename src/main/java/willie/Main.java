fa649316f25b42e047901ff9f7cf8bed9e4996be
theDAVID <thedavid5432@gmail.com>
2024-03-30 15:56:28 +0800

    input host information on start

--- src/main/java/willie/Main.java
+++ src/main/java/willie/Main.java
@@ -11,11 +11,11 @@ public class Main{
 	public static int port = 8080;
 	public static ConnectionThead connectionThead;
 	public static void main(String[] args){
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.print("Enter the host: ");
-//		host = scanner.nextLine();
-//		System.out.print("Enter the port: ");
-//		port = scanner.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the host: ");
+		host = scanner.nextLine();
+		System.out.print("Enter the port: ");
+		port = scanner.nextInt();
 		KeyUtils.generateKey();
 		connectionThead = new ConnectionThead(host, port);
 		connectionThead.start();
